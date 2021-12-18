13736aa05d86af2a0c6ab52d269c8f13275a26f2
DanielJang99 <daniel.jang.0621@gmail.com>
2021-12-19 03:13:53 +0900

    fix: metatags 수정

--- src/components/metatags/Metatags.jsx
+++ src/components/metatags/Metatags.jsx
@@ -11,6 +11,14 @@ function Metatags() {
                 property="og:description"
                 content="남성 브랜드 의류 대여 서비스, 렌타클로스"
             />
+            <meta
+                property="og:image"
+                content="https://rentaclothbucket.s3.ap-northeast-2.amazonaws.com/rentacloth_og.png"
+            />
+            <meta
+                property="og:image:secure_url"
+                content="https://rentaclothbucket.s3.ap-northeast-2.amazonaws.com/rentacloth_og.png"
+            />
         </>
     );
 }
