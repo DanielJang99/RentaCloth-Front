fb05bf6f590ac71c8743894e525b51aac7ac6edc
DanielJang99 <daniel.jang.0621@gmail.com>
2021-12-19 03:25:41 +0900

    chore: 메타태그 사이트 제목 추가

--- src/components/metatags/Metatags.jsx
+++ src/components/metatags/Metatags.jsx
@@ -3,6 +3,8 @@ import React from "react";
 function Metatags() {
     return (
         <>
+            <meta property="og:title" content="렌타클로스" />
+            <meta property="og:site_name" content="렌타클로스" />
             <meta
                 name="description"
                 content="남성 브랜드 의류 대여 서비스, 렌타클로스"
