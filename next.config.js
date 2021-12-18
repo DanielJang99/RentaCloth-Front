5c5f1b7452c3471e0c5660f07ed07cf8e545ae34
DanielJang99 <daniel.jang.0621@gmail.com>
2021-12-19 00:09:17 +0900

    환경 세팅

--- next.config.js
+++ next.config.js
@@ -1,11 +1,27 @@
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-    reactStrictMode: true,
-    pwa: {
-        dest: "public",
-        skipWaiting: true,
-        register: true,
-        disable: process.env.NODE_ENV === "development",
-    },
-});
+module.exports = function (...args) {
+    let original = require("./next.config.original.1639328369075.js");
+    const finalConfig = {};
+    const target = { target: "serverless" };
+    if (
+        typeof original === "function" &&
+        original.constructor.name === "AsyncFunction"
+    ) {
+        // AsyncFunctions will become promises
+        original = original(...args);
+    }
+    if (original instanceof Promise) {
+        // Special case for promises, as it's currently not supported
+        // and will just error later on
+        return original
+            .then((originalConfig) =>
+                Object.assign(finalConfig, originalConfig),
+            )
+            .then((config) => Object.assign(config, target));
+    } else if (typeof original === "function") {
+        Object.assign(finalConfig, original(...args));
+    } else if (typeof original === "object") {
+        Object.assign(finalConfig, original);
+    }
+    Object.assign(finalConfig, target);
+    return finalConfig;
+};
